1a7451139e6b113d35ba77e292d9544c838d002d
Paul Livingstone <theflyer1983@gmail.com>
2023-02-03 12:25:17 +0000

    1-5 Loading Course Data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,14 +1,26 @@
-<script setup lang='ts'>
+<script setup lang="ts">
+const course = useCourse();
+const route = useRoute();
+console.log(course);
 
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value?.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
 </script>
 
 <template>
-    <h2>Lesson</h2>
-    <p>This is a lesson</p>
-    <p>Chapter slug: {{ $route.params.chapterSlug}}</p>
-    <p>Lesson slug: {{ $route.params.lessonSlug}}</p>
-  </template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+  <p>{{ chapter?.title }}</p>
+  <p>{{ lesson?.title }}</p>
+</template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
