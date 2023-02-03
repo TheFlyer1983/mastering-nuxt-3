e76d9cb5b7e1fdb981787e397fb5dad5a89fc6c5
Paul Livingstone <theflyer1983@gmail.com>
2023-02-03 12:08:29 +0000

    1-4 Dynamic Routing

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -5,7 +5,9 @@
 <template>
     <h2>Lesson</h2>
     <p>This is a lesson</p>
-</template>
+    <p>Chapter slug: {{ $route.params.chapterSlug}}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug}}</p>
+  </template>
 
 <style lang="scss" scoped>
 
