c3db7b40eacaf1660551417bcac9bdbb185f6f5b
Paul Livingstone <theflyer1983@gmail.com>
2023-02-06 14:35:38 +0000

    4-2 Create a login page

--- pages/course.vue
+++ pages/course.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const { chapters } = useCourse();
+const { chapters, title } = useCourse();
 
 const resetError = (error: any) => {
   error.value = null;
@@ -7,57 +7,56 @@ const resetError = (error: any) => {
 </script>
 
 <template>
-  <div class="prose mb-12">
-    <h1>
-      <span class="font-medium">
-        Course:
-        <span class="font-bold">Mastering Nuxt 3</span>
-      </span>
-    </h1>
-  </div>
-
-  <div class="flex flex-grow flex-row justify-center">
-    <div
-      class="prose mr-4 flex min-w-[20ch] max-w-[30ch] flex-col rounded-md bg-white p-8"
-    >
-      <h3>Chapters</h3>
-      <div
-        class="mb-4 flex flex-col space-y-1"
-        v-for="chapter in chapters"
-        :key="chapter.slug"
-      >
-        <h4>{{ chapter.title }}</h4>
-        <NuxtLink
-          v-for="(lesson, index) in chapter.lessons"
-          :key="lesson.slug"
-          class="prose-sm flex flex-row space-x-1 py-1 font-normal no-underline"
-          :to="lesson.path"
-          :class="{ 'text-blue-500': lesson.path === $route.path }"
-        >
-          <span class="text-gray-500"> {{ index + 1 }}. </span>
-          <span>{{ lesson.title }}</span>
-        </NuxtLink>
-      </div>
+  <div>
+    <div class="prose mb-12">
+      <h1>
+        <span class="font-bold">{{ title }}</span>
+      </h1>
     </div>
 
-    <div class="prose w-[65ch] rounded-md bg-white p-12">
-      <NuxtErrorBoundary>
-        <NuxtPage />
-        <template #error="{ error }">
-          <p>
-            Oh no, something went wrong with the lesson!
-            <code>{{ error }}</code>
-          </p>
-          <p>
-            <button
-              class="bg-gray-500 py-1 px-3 font-bold text-white hover:cursor-pointer"
-              @click="resetError(error)"
-            >
-              Reset
-            </button>
-          </p>
-        </template>
-      </NuxtErrorBoundary>
+    <div class="flex flex-grow flex-row justify-center">
+      <div
+        class="prose mr-4 flex min-w-[20ch] max-w-[30ch] flex-col rounded-md bg-white p-8"
+      >
+        <h3>Chapters</h3>
+        <div
+          class="mb-4 flex flex-col space-y-1"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="prose-sm flex flex-row space-x-1 py-1 font-normal no-underline"
+            :to="lesson.path"
+            :class="{ 'text-blue-500': lesson.path === $route.path }"
+          >
+            <span class="text-gray-500"> {{ index + 1 }}. </span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
+      </div>
+
+      <div class="prose w-[65ch] rounded-md bg-white p-12">
+        <NuxtErrorBoundary>
+          <NuxtPage />
+          <template #error="{ error }">
+            <p>
+              Oh no, something went wrong with the lesson!
+              <code>{{ error }}</code>
+            </p>
+            <p>
+              <button
+                class="bg-gray-500 py-1 px-3 font-bold text-white hover:cursor-pointer"
+                @click="resetError(error)"
+              >
+                Reset
+              </button>
+            </p>
+          </template>
+        </NuxtErrorBoundary>
+      </div>
     </div>
   </div>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { title } = useCourse();
+</script>
+
+<template>
+  <div class="prose h-9 w-full max-w-2xl">
+    <h1>Log in to {{ title }}</h1>
+    <button class="rounded bg-blue-500 py-2 px-4 font-bold text-white">
+      Log in with Github
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
