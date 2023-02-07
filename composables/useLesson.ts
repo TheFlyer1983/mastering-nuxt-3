2f09fadfb433dc5d10770aa9c699cab529b54b20
Paul Livingstone <theflyer1983@gmail.com>
2023-02-07 13:53:39 +0000

    5-7 useFetchWithCache

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,32 +1,9 @@
-import { StorageSerializers } from '@vueuse/core';
 import { LessonWithPath } from '~~/types/course';
 
 export const useLesson = async (chapterSlug: string, lessonSlug: string) => {
-  const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
-
-  //Use sessionStorage to cach the lesson data
-  const lesson = useSessionStorage<LessonWithPath>(url, null, {
-    // By passing null as default it can't automatically
-    // determine which serializer to use
-    serializer: StorageSerializers.object
-  });
-
-  if (!lesson.value) {
-    const { data, error } = await useFetch<LessonWithPath>(url);
-
-    if (error.value) {
-      throw createError({
-        ...error.value,
-        statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`
-      });
-    }
-
-    lesson.value = data.value;
-  } else {
-    console.log(
-      `Getting lesson ${lessonSlug} in chapter ${chapterSlug} from cache`
-    );
-  }
+  const lesson = await useFetchWithCache<LessonWithPath>(
+    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+  );
 
   return lesson;
 };
